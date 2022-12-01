cb682f4a25b199517e67506f24a70a9fac98a933
Giulia Colombo <giulia.colombo.127@gmail.com>
2022-12-01 20:55:55 +0100

    unable to solve lab right now

--- js/index.js
+++ js/index.js
@@ -1,7 +1,43 @@
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const roadImg = new Image();
+roadImg.src = "./images/road.png"
+
+const carImg = new Image();
+carImg.src = "./images/car.png"
+
+const myObstacles = [];
+
+function drawBackground () {
+  ctx.drawImage(roadImg, 0, 0, 500, 700);
+}
+
+function drawPlayer () {
+  ctx.drawImage(carImg, 235, 640, 30, 60);
+}
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    console.log("click")
+    drawBackground(); //displays road
+    drawPlayer();
+  }
+
+  
+
 };
+
+class Car { //we create a player/car and set it at an initial position
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = 0;
+  }
+    
+} 
